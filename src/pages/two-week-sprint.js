277faee08f380ebb791d81bfda8795b159ce2b09
JeremyBatch <jdbatchelor85@gmail.com>
2019-12-17 10:59:24 -0600

    📖  DOC: move images to subfolder two-week-sprint, add two-week-sprint with html, rename class to className

---
+++ src/pages/two-week-sprint.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import '../components/layout.css';
+
+
+const TwoWeekSprintSite = () => (
+    <> 
+        content here
+    </>
+)
+
+
+export default TwoWeekSprintSite
